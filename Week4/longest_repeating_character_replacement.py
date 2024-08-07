76e3f9dea1ad4228cfcbd321d98bbc41b31043cc
Chase Agee <105754333+thechee@users.noreply.github.com>
2024-08-06 21:03:57 -0700

    finished longest_repeating_character

--- Week4/longest_repeating_character_replacement.py
+++ Week4/longest_repeating_character_replacement.py
@@ -1,15 +1,19 @@
 """
-ou are given a string s and an integer k. You can choose any character of the string and change it to any other uppercase English character. You can perform this operation at most k times.
+You are given a string s and an integer k. 
+You can choose any character of the string and change it to any other uppercase English character. 
+You can perform this operation at most k times.
 
-Return the length of the longest substring containing the same letter you can get after performing the above operations.
+Return the length of the longest substring containing the same letter
+you can get after performing the above operations.
 
- 
 
 Example 1:
 
 Input: s = "ABAB", k = 2
 Output: 4
 Explanation: Replace the two 'A's with two 'B's or vice versa.
+
+
 Example 2:
 
 Input: s = "AABABBA", k = 1
@@ -27,3 +31,27 @@ s consists of only uppercase English letters.
 """
 
 def character_replacement(s, k):
+  # use sliding window
+  # use a hash to keep counts
+  # return max len
+  l = 0
+  counts = {}
+  max_len = 0
+
+  # iterate over the string tracking r pointer
+  for r in range(len(s)):
+    # add the right pointer to the counts hash
+    counts[s[r]] = 1 + counts.get(s[r], 0)
+
+    # if the length of the window minus the maximum of the counts is greater than k
+    # slide the l pointer up and remove it's value from the counts hash
+    if r - l + 1 - max(counts.values()) > k:
+      counts[s[l]] -= 1
+      l += 1
+
+    max_len = max(max_len, r - l + 1)
+
+  return max_len
+
+print(character_replacement("ABBA", 2)) # 4
+print(character_replacement("AABABBA", 1)) # 4
