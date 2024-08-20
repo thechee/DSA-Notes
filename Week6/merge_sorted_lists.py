0c9d1a939664d588c5d7d764fae4d4b5726edcf1
Chase Agee <chase.agee@gmail.com>
2024-08-20 13:32:42 -0700

    completed and commented merge sorted list

--- Week6/merge_sorted_lists.py
+++ Week6/merge_sorted_lists.py
@@ -35,19 +35,31 @@ class ListNode:
         self.next = next
 
 def merge_two_lists(list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+    # make a dummy node, set current equal to dummy
     dummy = ListNode()
     current = dummy
 
+    # while the lists aren't empty
     while list1 and list2:
+        # if list1's val is less than list2's val
         if list1.val < list2.val:
+            # set current's next to list1
             current.next = list1
+            # set list1 to list1.next (incremnet along the list)
             list1 = list1.next
+        # else
         else: 
+            # set curr's next to list2
             current.next = list2
+            # increment along list2
             list2 = list2.next
-            
+        # set current to current.next (increment along the list)
+
+    # at the end of while loop, if there are still nodes in either list,
+    # set that to the curr's next
     current.next = list1 if list1 else list2
 
+    # return the dummy's next
     return dummy.next
     
 
